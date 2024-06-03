eb6b9cd9b2685653ca3fbf14816a267efc7b500e
Errol Kutan <errol.kutan@mongodb.com>
2024-06-03 12:13:42 -0400

    Updating testing code

--- mdbaas/test/utils/createprojects.py
+++ mdbaas/test/utils/createprojects.py
@@ -25,6 +25,51 @@ APPLICATION_ENVS = ["PROD", "UAT", "DEV"]
 ########################################################################################################################
 # Main Substantive Methods
 ########################################################################################################################
+def requestManualConfirmationFromUser(prompt, requiredResp):
+    """
+    Request Manual Confirmation from User
+
+    :param prompt:
+    :param requiredResp:
+    :return:
+    """
+    resp = input(prompt)
+    respInvalid = (resp != requiredResp)
+    while respInvalid:
+        print("Response is not valid. Please type {{}} to proceed or CTRL + C to exit".format(requiredResp))
+        resp = input(prompt)
+        respInvalid = (resp != requiredResp)
+
+
+def deleteProjectsInOrg(orgId, numProjects, startProjNum=0):
+    """
+    Delete Projects in Org
+
+    :param orgId:
+    :param numProjects:
+    :param startProjNum:
+    :return:
+    """
+    global opsMgrConnector
+    projectsToDelete = []
+    endProjNum = startProjNum + numProjects
+    for i in range(startProjNum, endProjNum):
+        projName = "project{}".format(i)
+        projectsToDelete.append(projName)
+
+    requiredResp = "Y"
+    requestManualConfirmationFromUser(
+        "Confirm you would like to delete projects project{} - project{}: Press {} to proceed...".format(startProjNum, endProjNum, requiredResp),
+        requiredResp
+    )
+
+    groupsInOrg = opsMgrConnector.getGroupsWithinOrganization(orgId)
+    for group in groupsInOrg["results"]:
+        if group["name"] in projectsToDelete:
+            opsMgrConnector.removeGroup(group["id"])
+            logging.info("Deleting group with name {} and id {}".format(group["name"], group["id"]))
+
+    logging.info("Done")
 
 def createProjectsInOrg(orgId, numProjects, startProjNum=0):
     """
@@ -83,6 +128,8 @@ def setupArgs():
     parser.add_argument('--startProjNum', required=False, action="store", dest='startProjNum', default=0,
                         help='The numerical id of first project to create. This is used so that you can create projects starting with the last project created in the most recent run of this app')
 
+    parser.add_argument('--cleanup', required=False, action="store_true", dest='cleanup', default=False,
+                        help='Include this flag to delete the projects in the specified range')
 
     parser.add_argument('--loglevel', required=False, action="store", dest='logLevel', default='info',
                         help='Log level. Possible values are [none, info, verbose]')
@@ -152,7 +199,10 @@ def main():
     # Get Ops Manager connection
     global opsMgrConnector
     opsMgrConnector = OpsMgrConnector(args.opsMgrUri, args.opsMgrApiUser, args.opsMgrApiKey)
-    createProjectsInOrg(args.orgId, int(args.numProjects), int(args.startProjNum))
+    if args.cleanup is not None and bool(args.cleanup):
+        deleteProjectsInOrg(args.orgId, int(args.numProjects), int(args.startProjNum))
+    else:
+        createProjectsInOrg(args.orgId, int(args.numProjects), int(args.startProjNum))
 
 # -------------------------------
 if __name__ == "__main__":
